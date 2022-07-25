bcc32a219adce8910599cbc4a19b6f8922f7fab4
Ashpak Mulani <ashpaklmulani@gmail.com>
2022-07-25 08:23:00 +0530

    card_drawer: [update] : Comments addition in css, javascript and readme

--- cards_drawer/readme.md
+++ cards_drawer/readme.md
@@ -9,23 +9,14 @@ GIF might not show all smooth animation correctly
 ![image](./CardDrawer.gif)
 
 
+## Code Execution
+
+- clone the code locally
+- open code in VSCode and make sure 'live server' extention is installed in vs code
+- right click on index.html file and choose 'Open with live server'
 
 
 
 ## Sample demo in video format
 
 https://user-images.githubusercontent.com/42534082/180659205-7fc54b01-8a30-414c-a1da-a0ca932da860.mp4
-
-
-
-
-
-## Code Execution
-
-- clone the code locally
-- open code in VSCode and make sure 'live server' extention is installed in vs code
-- write click on index.html file and choose 'Open with live server'
-
-code should run locally in browser. 
-
-Happy coding !!!
--- cards_drawer/script.js
+++ cards_drawer/script.js
@@ -1,17 +1,18 @@
+/* select all items with class card so that on click event listrner
+can be attached on individual card item */ 
 const card_list = document.querySelectorAll(".card");
 
-console.log(card_list);
-
+/* add on click event listner on each card to insert 'active' css class
+which makes card bigger in size as compared to other for expanded view */
 card_list.forEach(card => {
-
     card.addEventListener("click", ()=>{
         removeActive();
-        card.classList.add("active");
-    
+        card.classList.add("active");    
     })    
 });
 
-
+/* seperate function to remove 'active' class on each card before adding 'active'
+class on new card to make sure only one card is active */
 const removeActive = ()=> {
     card_list.forEach(card => {
     card.classList.remove("active");
