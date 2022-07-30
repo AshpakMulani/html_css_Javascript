c76c779c0cceab9c168a12679dae4617c1661811
Ashpak Mulani <ashpaklmulani@gmail.com>
2022-07-30 10:50:54 +0530

    scroll_display_animation: [Feature] : Scroll animation and lazy loading implimentation using IntersectionObserver

--- scroll_display_animation/readme.md
+++ scroll_display_animation/readme.md
@@ -8,14 +8,21 @@ ref : https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClient
 
 function returns DomRect object about size and position. This object can be scanned to get current position of object and check if it has reached viewport. Custom calculations needs to be performed to udnerstand objects locaiton compared to viewport.
 
-First 6 objects on page are animated with this type of implimentation 
+First 5 objects light blue background on page are animated with this type of implimentation 
+
+
+
+
+## intersectionObserver API
+ref : https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+used for lazy loading, nfinite scrolling, mnaging visibility etc..It helps to understand if object is in viewport or not.
+
+The Intersection Observer API provides a way to asynchronously observe changes for the intersection of a target element with an ancestor element or with a top-level document’s viewport. (ref. MDN docs)
+
+Later 2 object lightpink and light gray background are showung visibility and lazy loading managment using IntersectionObserver
 
 GIF might not show all smooth animation correctly
-
-![image](./getBoundingClientRect.gif)
-
-
-
 
 
 
--- scroll_display_animation/script.js
+++ scroll_display_animation/script.js
@@ -1,17 +1,22 @@
-const items = document.querySelectorAll(`[class*="_container"]`)
+const items_all = [...document.querySelectorAll(`[class*="_container"]`)]
+
+// select first 5 elements (till fourth_container) starting from index zero.
+const items_rect = items_all.slice(0,5)
+
+console.log(items_rect)
 
 // on each scroll event on window call checkVisibility() function
-window.addEventListener('scroll', checkVisibility)
+window.addEventListener('scroll', checkRectVisibility)
 
-checkVisibility()
+checkRectVisibility()
 
-function checkVisibility() {
+function checkRectVisibility() {
     //deviding screen height in 5 parts and triggering animation
     //when scrolle item cross last one bottom part 
     const triggerBottom = window.innerHeight / 5 * 4
 
     //loop through each container
-    items.forEach(box => {
+    items_rect.map((box) => {
         //get boxtop of each box on every scroll action
         const boxTop = box.getBoundingClientRect().top
         
@@ -27,5 +32,54 @@ function checkVisibility() {
             box.classList.add('hide')
             box.classList.remove('visible')
         }
-    })
+    });
 }
+
+// ****************************  Intersection Observer implimentation **********************
+
+function manageIntersectionVisibility(entries) {
+    // all elements being observed are sent as input to callback function
+    // loop through all element to check if anything is visible yet in viewport
+    entries.map((entry) => {
+      // check if it is intersecting with viewport using buildin property
+      // and add visible class if being intersected
+   
+      if (entry.isIntersecting) {
+      
+        if (String(entry.target.className).includes("lazyload")){                    
+          entry.target.src = entry.target.dataset.src;
+          observer.unobserve(entry.target);
+          entry.target.parentElement.classList.add('visible');
+          entry.target.parentElement.classList.remove('hide');          
+          entry.target.classList.remove('hide')
+        }
+        else{    
+        entry.target.classList.remove('hide')
+        entry.target.classList.add('visible')
+        console.log(entry)      
+        }  
+      }
+      else {
+        entry.target.classList.remove('visible')
+        entry.target.classList.add('hide')
+      }
+    });
+  }
+ 
+  /*
+  can be used to control margin or intersection threshold 
+
+  let options = {
+    root: document.querySelector(':root'),
+    rootMargin: '0px',
+    threshold: 1.0
+  }
+  */  
+  
+  const observer = new IntersectionObserver(manageIntersectionVisibility);
+
+  // add observer for sixth_container
+  
+  observer.observe(items_all[5]);
+
+  observer.observe(items_all[6].firstElementChild);
